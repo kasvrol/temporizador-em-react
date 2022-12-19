7a0f4c9aadb1190c6dc2d5a5584e6f047deab1f9
ana.karolina <ana.karolina@ablsystem.com.br>
2022-12-19 18:13:57 -0300

    feat: importação styles/global.css e uso da biblioteca tailwind para estilização dos componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react';
-import './index.css';
+import "./styles/global.css"
 
 function App() {
     const [count, setCount] = useState(60)
-    const [action, setAction] = useState("start")
+    const [action, setAction] = useState("Start")
     const [start, setStart] = useState(false)
     const [restartNow, setRestartNow] = useState(false)
 
@@ -16,20 +16,20 @@ function App() {
     }, [count, start, restartNow])
 
     const buttonAction = () => {
-        if (action === 'start') {
-            setAction('pause')
+        if (action === 'Start') {
+            setAction('Pause')
             setRestartNow(false)
             setStart(true)
         } else {
-            setAction('start')
+            setAction('Start')
             setRestartNow(false)
             setStart(false)
         }
     }
 
     const restart = () => {
-        if (action !== 'start') {
-            setAction('start')
+        if (action !== 'Start') {
+            setAction('Start')
             setStart(false)
             setRestartNow(true)
         } else {
@@ -39,11 +39,16 @@ function App() {
     }
 
     return (
-        <div>
-            <div>{count}</div>
-            <button onClick={() => buttonAction()}>{action}</button>
-            <button onClick={() => restart()}>restart</button>
-        </div>
+        <div className="flex items-center justify-center w-full h-full bg-yellow-200">
+            <section className="flex flex-col items-center justify-evenly  w-80 h-64 bg-orange-500 rounded-lg ">
+                <p className="font-extralight  text-8xl text-slate-200">{count}</p>
+                <section className="flex flex-row items-center justify-evenly  w-52 ">
+                    <button className=" bg-orange-700 w-20 h-8 rounded-lg text-lg text-slate-200" onClick={() => buttonAction()}>{action}</button>
+                    <button className="bg-orange-700  w-20 h-8 rounded-lg text-lg text-slate-200" onClick={() => restart()}>Restart</button>
+                </section>
+            </section>
+
+        </div >
     );
 }
 
